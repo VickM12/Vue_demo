1f31f2e37be273c7869e1501813564e02add9df3
Vicktor M <vicktor.moberg@gmail.com>
2021-01-04 10:01:12 -0500

    added methods to add or remove item from cart and mouse over events

--- main.js
+++ main.js
@@ -7,5 +7,31 @@ var app = new Vue({
         inStock: true,
         inventory: 10,
         onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+              variantId: 2234,
+              variantColor: 'green',
+              variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'    
+            },
+            {
+              variantId: 2235,
+              variantColor: 'blue',
+              variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+            }
+          ],
+          sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+          cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart +=1
+        },
+        updateProduct(variantImage){
+            this.image = variantImage
+        },
+        removeFromCart() {
+            this.cart -= 1
+        }
     }
 })
