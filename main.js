b6d8fa4725516808b0c9717e368ea38ea78c4f65
Vicktor M <vicktor.moberg@gmail.com>
2021-01-04 09:34:46 -0500

    Added 'conditional' rendering of properties

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: 'assets/vmSocks-green-onWhite.jpg',
-        imgHref: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+        imgHref: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        inStock: true,
+        inventory: 10,
+        onSale: true,
     }
 })
